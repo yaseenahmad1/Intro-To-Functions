5fd01665e96ba3a004cdd2a6a286985981ca5681
yaseenahmad1 <yahmad09@gmail.com>
2024-09-11 15:35:32 -0700

    problems solved with questions for second problem

--- problems/02-repeat-name.js
+++ problems/02-repeat-name.js
@@ -4,15 +4,24 @@ by calling the repeatName(name) function so that the console.logs will
 print out the expected values below.
 */
 
+//My function needs a name and, in this case, a parameter.
 function repeatName(name) {
-  return name + " " + name;
-}
+  return name + " " + name; //Once this function is invoked, it should
+  // return the argument as a concatenated string of the argument repeated
+  //with a space in between. 
+} 
 
-// Your code here 
+//I then assign my functions to the variables below so that by simply console  
+//logging the variables, I will get the desired output.
+let repeatDaniel = repeatName(); //why is it not repeatName(name)?
+let repeatMark = repeatName();  //why did I get three green checks without 
+let repeatBeyonce = repeatName(); //assigning the functions to the variables?
 
-let repeatDaniel = repeatName();
-let repeatMark = repeatName();
-let repeatBeyonce = repeatName();
+//Questions to ask : when you create a variable outside your function, 
+//to perform that function, does that variable become the argument?
+//How did I pass when the third name prints out exclaimation marks. 
+//I was going to use variables and an if/else statement 
+//specifically for Beyonce to ensure the function logs line 24. 
 
 console.log(repeatDaniel);  // => "Daniel Daniel"
 console.log(repeatMark);    // => "Mark Mark"
